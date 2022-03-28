2364ced81b549044089118fb4b8ba02281b5e7c4
fnicom <fnicomedes@gmail.com>
2022-03-28 16:56:53 -0300

    add estilo

--- src/components/ListaFrutas.jsx
+++ src/components/ListaFrutas.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import AdicionaFruta from './AdicionaFruta';
-import Fruta from './Fruta'
+import Fruta from './Fruta';
+import { useSelector } from 'react-redux';
 
 const ListaFrutas = () => {
 
-    const frutas = [
-        { id: 1, nome: 'Abacaxi', quatidade: 5 },
-        { id: 2, nome: 'Uva', quantidade: 12 }
-    ]
+    const frutas = useSelector(state => state.frutaReducers.frutas)
 
   return (
     <>
